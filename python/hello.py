a5fb31b164258b247f05e334332f10af7bb0891c
Pontus Pålsson <pontus.palsson@learnet.se>
2020-12-03 16:28:26 +0100

    tränat på if och else funktioner även &&

---
+++ python/hello.py
@@ -0,0 +1,8 @@
+print("How much calories for breakfast?:")
+breakfast = int(input())
+print("How much calories for lunch?:")
+lunch = int(input())
+print("How much calories for dinner?:")
+dinner = int(input())
+sum = breakfast + lunch + dinner
+print(sum)
